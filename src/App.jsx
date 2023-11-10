cedad5df13d139fe979ed5c3ab710e855222d302
= <=>
2023-11-10 03:19:34 +0300

    inputtan veri cekme eklendı baya kotu ustune calısılacak

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
 import axios from "axios"
+import { useEffect, useRef, useState } from "react"
 
 const App = () => {
-	const location = "Ankara"
+	const [data, setData] = useState({})
+	const myInput = useRef()
+	const [location, setLocation] = useState("")
+
 	const url = `https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=${location}`
 	const headers = {
 		Authorization: "apikey 6x4Y2RR863L78oZhHm6G8c:7F7qeeju2ZqLyxuL6MrgSX",
 		"Content-Type": "application/json",
 	}
-	axios
-		.get(url, { headers: headers })
-		.then(response => {
-			console.log(response.data)
-		})
-		.catch(error => {
-			console.error(error)
-		})
+	useEffect(() => {
+		axios
+			.get(url, { headers: headers })
+			.then(response => {
+				setData(response.data, response)
+				console.log(response.data)
+			})
+			.catch(error => {
+				console.error(error)
+			})
+	}, [location])
 
 	return (
 		<div className="w-full h-full relative">
 			<div className="text-center p-4 ">
 				<input
+					ref={myInput}
+					onChange={() => setLocation(myInput.current.value)}
 					placeholder="Enter Location"
 					className="bg-white-600/100 shadow-md py-3 px-6 w-[700px] text-lg rounded-3xl border-gray-300 placeholder:text-gray-400 focus:outline-none"
 				/>
